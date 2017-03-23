e8f8a898d7909eb00e2b8cc0a896392ce1ae7295
Keith Norris <keith@spectrum-nashville.com>
2017-03-23 08:34:11 -0700

    Testing text.exs database configuration for semaphore

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :chs, Chs.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "runner",
+  password: "semaphoredb",
   database: "chs_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
